90c1c6ab8009c87d6874017dd5f07ed164b0e0a7
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-12-16 00:40:31 +0900

    create project

---
+++ app.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT;
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Application listening at ${PORT}`);
+});
